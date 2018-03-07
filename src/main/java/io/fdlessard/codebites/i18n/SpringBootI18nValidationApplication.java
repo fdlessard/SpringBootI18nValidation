4ab0e081d0723c7b7f3320e2a1814eff3e253837
fdlessard <fdlessard@gmail.com>
2018-03-06 23:24:59 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/i18n/SpringBootI18nValidationApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootI18nValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootI18nValidationApplication.class, args);
+	}
+}
