8a65ee75c867fde6992bc841915ac0c929e4aeeb
fdlessard <fdlessard@gmail.com>
2018-03-07 22:19:15 -0500

    Added validation

--- src/main/java/io/fdlessard/codebites/i18n/controllers/CustomerController.java
+++ src/main/java/io/fdlessard/codebites/i18n/controllers/CustomerController.java
@@ -1,4 +1,20 @@
 package io.fdlessard.codebites.i18n.controllers;
 
+
+import io.fdlessard.codebites.i18n.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
 public class CustomerController {
+
+    @PostMapping(path = "/customer", consumes = "application/json", produces = "application/json")
+    public void createCustomer(@Valid @RequestBody Customer customer) {
+        log.info("CustomerController.createCustomer({})", customer);
+    }
 }
--- src/main/java/io/fdlessard/codebites/i18n/domain/Customer.java
+++ src/main/java/io/fdlessard/codebites/i18n/domain/Customer.java
@@ -1,4 +1,25 @@
 package io.fdlessard.codebites.i18n.domain;
 
-public class Customer {
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer implements Serializable {
+
+    private String id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull(message = "{customerController.lastName.notNull}")
+    private String lastName;
+
+    private String company;
+
 }
